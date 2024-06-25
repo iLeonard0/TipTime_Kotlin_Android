dc490467cad13a201854e27a09dea4bfbe431412
iLeonard0 <folleleonardoprogramming@gmail.com>
2024-06-25 20:01:04 -0300

    Kotlin - Android, TIP TIME

--- app/src/androidTest/java/unipar/edu/utfpr/tiptime/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/unipar/edu/utfpr/tiptime/ExampleInstrumentedTest.kt
@@ -10,35 +10,27 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
-import org.hamcrest.CoreMatchers.containsString
 import org.junit.runner.RunWith
 import org.junit.Rule
+import org.junit.matchers.JUnitMatchers.containsString
 
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-
     @get:Rule()
-    val activity = ActivityScenarioRule(MainActivity::class.java)
-
+    val activity = ActivityScenarioRule( MainActivity::class.java )
     @Test
     fun useAppContext() {
 
-        onView(withId(R.id.cost_of_service))
-            .perform(typeText("50,00"))
-            .perform(ViewActions.closeSoftKeyboard())
+        onView( withId( R.id.cost_of_service ) )
+            .perform( typeText( "50.00") )
+            .perform( ViewActions.closeSoftKeyboard() )
 
-        onView(withId(R.id.calculate_button))
-            .perform(click())
+        onView( withId( R.id.calculate_button ) )
+            .perform( click() )
 
-        onView(withId(R.id.tip_result))
-            .check(matches(withText(containsString("10,00"))))
+        onView( withId( R.id.tip_result ) )
+
+            .check( matches( withText( containsString( "10,00") ) ) )
+
     }
-
-
 }
